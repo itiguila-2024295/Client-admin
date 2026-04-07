2664d11c93b45675b87e9de50f299332ed719476
itiguila-2024295 <itiguila-2024295@kinal.edu.gt>
2026-04-07 17:17:53 -0600

    feat: trabajo del 7 de abril

--- src/features/auth/components/ForgotPassworForm.jsx
+++ src/features/auth/components/ForgotPassworForm.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
 
-export const ForgotPassworForm = () => {
+export const ForgotPassworForm = ({ onSwitch }) => {
   return (
-    <div>ForgotPassworForm</div>
+    <form action="" className='space-y-5'>
+        <div>
+          <label htmlFor="" className='block text-sm font-medium text-gray-800 mb-1.5'>
+            Email
+          </label>
+          <input 
+            type="email"
+            placeholder='correo@ejemplo.com'
+            className='w-full px-3 py-2 border rounded-lg'  
+          />
+        </div>
+        <button
+          type='submit'
+          className='w-full bg-main-blue text-white  py-2 rounded-lg disabled:opacity50 hover:opacity-90 cursor-pointer'
+        >
+          Enviar Correo
+        </button>
+        <p className='text-center text-sm text-gray-600'>
+          ¿Recordates tu contraseña?{" "}
+          <button
+            type='button'
+            className='text-main-blue font-medium hover:opacity-80 cursor-pointer'
+            onClick={onSwitch}
+          >
+            Iniciar Sesión
+          </button>
+        </p>
+    </form>
   )
 }
--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const LoginForm = () => {
+export const LoginForm = ({ onForgot }) => {
   return (
     <form action="" className='space-y-5'>
         <div>
-            <label htmlFor="" className='block text-sm font-medium text-gray-800 mb-1.5'>
+            <label htmlFor="emailOrUsername" className='block text-sm font-medium text-gray-800 mb-1.5'>
                 Email o Usuario
             </label>
             <input 
@@ -14,6 +14,29 @@ export const LoginForm = () => {
                 className='w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus-ring 2 focus:ring-blue-500'
             />
         </div>
+        <div>
+            <label htmlFor="password" className='block text-sm font-medium text-gray-800 mb-1.5'>
+                Contraseña
+            </label>
+            <input 
+                type="password" 
+                id='password'
+                placeholder='••••••••'
+                className='w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring focus:ring-blue-500'
+            />
+        </div>
+        <button type='submit' className='w-full py-2 px-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300 cursor-pointer text-sm font-medium'>
+            Iniciar Sesión
+        </button>
+        <p className='text-center text-sm'>
+            <button 
+                type='button' 
+                onClick={onForgot}
+                className='text-main-blue hover:underline cursor-pointer text-sm' 
+            >
+                ¿Olvidaste tu contraseña?
+            </button>
+        </p>
     </form>
   )
 }
