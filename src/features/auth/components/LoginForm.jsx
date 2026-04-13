e6fa8d3748a1f21adb4bd14e510596cdabd7f354
itiguila-2024295 <itiguila-2024295@kinal.edu.gt>
2026-04-13 17:29:35 -0600

    Trabajo 13 de arbil

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { useAuthStore } from '../store/authStore';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginForm = ({ onForgot }) => {
+
+    const navigate = useNavigate();
+
+    const login = useAuthStore((state) => state.login);
+    const loading = useAuthStore((state) => state.loading);
+    const error = useAuthStore((state) => state.error);
 
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = (data) => {
-        //Mndar información al backedn para iniciar sesión
+        // Mandar información al backend para iniciar sesión
         console.log(data);
+        const res = login(data);
+        if (res.success) {
+            navigate("/dashboard");
+        }
     }
 
     return (
@@ -38,12 +50,21 @@ export const LoginForm = ({ onForgot }) => {
                     className='w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring focus:ring-blue-500'
                     {...register('password', { required: "La contraseña es obligatoria" }) }
                 />
-                {errors.password && (
-                    <p className='text-red-500 text-sm mt-1'>{errors.password.message}</p>
-                )}
+                {errors.password && 
+                    <p className='text-red-600 text-xs mt-1'>{errors}</p>
+                }
             </div>
-            <button type='submit' className='w-full bg-main-blue text-white  py-2 rounded-lg disabled:opacity50 hover:opacity-90 cursor-pointer'>
-                Iniciar Sesión
+
+            {error && (
+                <p className='text-red-500 text-sm mt-1'>{error}</p>
+            )}
+
+            <button     
+                type='submit' 
+                className='w-full bg-main-blue text-white  py-2 rounded-lg disabled:opacity50 hover:opacity-90 cursor-pointer'
+                disabled = {loading}
+            >
+                {loading ? "Iniciando..." : "Iniciar sesión"}
             </button>
             <p className='text-center text-sm'>
                 <button
--- src/features/auth/store/authStore.js
+++ src/features/auth/store/authStore.js
@@ -1,3 +1,41 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { login } from "../../../shared/api";
+import { login as loginRequest } from "../../../shared/api";
+
+export const useAuthStore = create(
+    persist(
+        (set, get) => ({
+            user: null,
+            token: null,
+            expiresAt: null,
+            loading: false,
+            error: null,
+            isAutenticated: false,
+
+            login: async ({ emailOrUsername, password }) => {
+                try {
+                    set({ loading: true, error: null });
+
+                    const { data } = await loginRequest({ emailOrUsername, password });
+
+                    set({
+                        user: data.userDatils,
+                        token: data.token,
+                        expiresAt: data.expiresAt,
+                        loading: false,
+                    })
+
+                    return { success: true };
+
+                } catch (err) {
+                    console.error("Login error:", err);
+                    const message = 
+                        err.response?.data?.message || "Error de autenticación."
+                    set({ error: message, loading: false });
+                    return { success: false, error:message };
+                }
+            }
+        }),
+        { name: "auth-storage"}
+    )
+)
